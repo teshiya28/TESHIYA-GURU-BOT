362e16deb5e8ce496ff5015668ad33ab360f817f
teshiya28 <103806701+teshiya28@users.noreply.github.com>
2023-06-24 21:16:39 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497079', 'sandaru', true],
-  ['6281255369012', 'Ash', true], 
+  ['94776069537', 'sandaru', true],
+  ['94776069537', 'TESHIYA', true], 
   [''] 
 ] //Number of owners
 
